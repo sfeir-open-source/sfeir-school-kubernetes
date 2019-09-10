78f6267e40d037ea395e372e83e4c7aafcbdf874
Antoine Meausoone <antoine.meausoone@gmail.com>
2019-09-10 17:09:31 +0200

    feat(terraform): script to create namespace

--- terraform/gke/create-namespace.sh
+++ terraform/gke/create-namespace.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
 function createNamespace(){
-  namespace="namespace-${1}"
+  namespace="${1}"
   kubectl create namespace ${namespace} --dry-run -o yaml | kubectl apply -f -
 }
+
+for i in $toComplete ; do
+  createNamespace $i
+done
 
